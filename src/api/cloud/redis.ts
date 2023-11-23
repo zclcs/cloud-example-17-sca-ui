c71ff574f5f25cecd7e590b004d72bd77837ef3e
zclcs <2371219112@qq.com>
2023-11-23 10:30:43 +0800

    修复redis key 带斜杠导致接口请求不到

--- src/api/cloud/redis.ts
+++ src/api/cloud/redis.ts
@@ -1,16 +1,21 @@
+import { ContentTypeEnum } from '/@/enums/httpEnum';
 import { defHttpWithTransform } from '/@/utils/http/axios';
 
 enum Api {
   Redis = '/maintenance/redis',
 }
 
-export const deleteRedisApi = (ids: String) =>
+export const deleteRedisApi = (params: String) =>
   defHttpWithTransform.delete<void>({
-    url: Api.Redis + '/' + ids,
+    url: Api.Redis,
+    headers: {
+      'Content-Type': ContentTypeEnum.JSON,
+    },
+    data: params,
   });
 
 export const getRedisPage = (params: any) =>
   defHttpWithTransform.get<any>({ url: Api.Redis, params });
 
 export const getRedis = (key: string) =>
-  defHttpWithTransform.get<any>({ url: Api.Redis + '/one' + '/' + key });
+  defHttpWithTransform.get<any>({ url: Api.Redis + '/one', params: { key: key } });
